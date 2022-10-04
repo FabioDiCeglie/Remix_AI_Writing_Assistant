c195ef9185bba70a25eafec4cb0f4655b9d9549a
FabioDiCeglie <dcfabio96@gmail.com>
2022-10-04 20:39:53 +0200

    feat: adjust login and logout

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,17 +1,36 @@
-import { Link } from "@remix-run/react";
+import { Form, Link } from "@remix-run/react";
 import { useOptionalUser } from "~/utils";
 
 export default function Index() {
   const user = useOptionalUser();
   return (
     <div>
-      <h1 className="text-slate-100">This is the index page</h1>
       {user ? (
-        <p className="text-slate-300">Welcome {user.email}</p>
+        <div className="text-slate-200">
+          <p>Welcome {user.email}</p>
+          <Form action="/logout" method="post">
+            <button
+              type="submit"
+              className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            >
+              Log out
+            </button>
+          </Form>
+        </div>
       ) : (
-        <div className="flex gap-5">
-          <Link to="/join">Join</Link>
-          <Link to="/login">Login</Link>
+        <div className="flex gap-5 text-slate-200">
+          <Link
+            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            to="/join"
+          >
+            Join
+          </Link>
+          <Link
+            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            to="/login"
+          >
+            Login
+          </Link>
         </div>
       )}
     </div>
