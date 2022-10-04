61a112a5114ae99f03fbc916458d9207bcf413c3
FabioDiCeglie <dcfabio96@gmail.com>
2022-10-04 20:45:12 +0200

    feat: create the homepage

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,35 +4,40 @@ import { useOptionalUser } from "~/utils";
 export default function Index() {
   const user = useOptionalUser();
   return (
-    <div>
-      {user ? (
-        <div className="text-slate-200">
-          <p>Welcome {user.email}</p>
-          <Form action="/logout" method="post">
-            <button
-              type="submit"
+    <div className="flex min-h-screen items-center justify-center">
+      <header className="max-w-3xl">
+        <h1 className="text-4xl font-bold text-slate-100">
+          Welcome to the most awesome AI Writing Assistant
+        </h1>
+        {user ? (
+          <div className="mx-auto flex w-full text-slate-200 ">
+            <p>Welcome {user.email}</p>
+            <Form action="/logout" method="post">
+              <button
+                type="submit"
+                className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+              >
+                Log out
+              </button>
+            </Form>
+          </div>
+        ) : (
+          <div className="flex gap-5 text-slate-200">
+            <Link
               className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+              to="/join"
             >
-              Log out
-            </button>
-          </Form>
-        </div>
-      ) : (
-        <div className="flex gap-5 text-slate-200">
-          <Link
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-            to="/join"
-          >
-            Join
-          </Link>
-          <Link
-            className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-            to="/login"
-          >
-            Login
-          </Link>
-        </div>
-      )}
+              Join
+            </Link>
+            <Link
+              className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+              to="/login"
+            >
+              Login
+            </Link>
+          </div>
+        )}
+      </header>
     </div>
   );
 }
