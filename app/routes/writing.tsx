d817cf2fd704a3b62209fa90be36522d325dff35
FabioDiCeglie <dcfabio96@gmail.com>
2022-10-05 19:45:45 +0200

    fix: create a loaderFunction

--- app/routes/writing.tsx
+++ app/routes/writing.tsx
@@ -1,7 +1,15 @@
 import { Form, Link } from "@remix-run/react";
 import { useUser } from "~/utils";
+import { requireUserId } from "~/session.server";
+import { json, LoaderFunction } from "@remix-run/server-runtime";
 
-export default function Index() {
+export const loader: LoaderFunction = async ({ request }) => {
+  const userId = await requireUserId(request);
+
+  return json({ ok: true });
+};
+
+export default function Writing() {
   const user = useUser();
   return <div className="text-slate-100">{user.email}</div>;
 }
