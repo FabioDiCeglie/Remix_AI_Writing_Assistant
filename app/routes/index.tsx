9e691a65d62b3eb7a02f1de70ba0c24c83a51bfc
FabioDiCeglie <dcfabio96@gmail.com>
2022-10-05 19:35:17 +0200

    working in a new page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -13,12 +13,20 @@ export default function Index() {
           <div className="mx-auto mt-4 flex w-full items-center justify-between px-20 text-slate-200">
             <p>Welcome {user.email}</p>
             <Form action="/logout" method="post">
-              <button
-                type="submit"
-                className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
-              >
-                Log out
-              </button>
+              <div className="flex gap-5">
+                <Link
+                  className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+                  to="/"
+                >
+                  Start Writing
+                </Link>
+                <button
+                  type="submit"
+                  className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+                >
+                  Log out
+                </button>
+              </div>
             </Form>
           </div>
         ) : (
