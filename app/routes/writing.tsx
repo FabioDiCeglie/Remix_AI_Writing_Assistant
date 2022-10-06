d9bf7bb9122334cefc7315f0069a9d6d92e7d604
FabioDiCeglie <dcfabio96@gmail.com>
2022-10-06 18:43:26 +0200

    feat: created input and button submit

--- app/routes/writing.tsx
+++ app/routes/writing.tsx
@@ -37,8 +37,25 @@ export default function Writing() {
             name="prompt"
             id="prompt"
             rows={5}
-            className="w-full rounded-sm bg-slate-800 text-slate-200"
+            className="w-full rounded-sm bg-slate-800 p-4 text-slate-200"
           ></textarea>
+
+          <div className="flex">
+            <input
+              type="number"
+              name="tokens"
+              id="tokens"
+              defaultValue={150}
+              className="w-24 rounded-sm bg-slate-800 p-4 text-slate-200"
+            />
+            <button
+              type="submit"
+              className="ml-4 rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+            >
+              {" "}
+              Submit
+            </button>
+          </div>
         </fieldset>
       </Form>
     </div>
