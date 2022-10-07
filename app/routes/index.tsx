e02b0dd58ba8c9fa30c6e5b31da601429ab3e542
FabioDiCeglie <dcfabio96@gmail.com>
2022-10-07 15:15:02 +0200

    fix: fixed some styling

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -15,7 +15,7 @@ export default function Index() {
             <div className="flex gap-5">
               <Form action="/logout" method="post">
                 <Link
-                  className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+                  className="mr-3 rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
                   to="/writing"
                 >
                   Start Writing
